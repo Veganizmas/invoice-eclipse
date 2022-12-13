04db17cb164c93452d66892e32f12898c785e4e5
BitinasDC <bitinas@live.com>
2022-12-13 15:26:27 +0200

    Added more data to Swagger

--- Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/configs/SpringFoxConfig.java
+++ Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/configs/SpringFoxConfig.java
@@ -1,10 +1,14 @@
 package lt.codeacademy.invoice.configs;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,7 +20,19 @@ public class SpringFoxConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(ourApiInfo());
+    }
+    
+    private ApiInfo ourApiInfo() {
+        return new ApiInfo(
+          "Invoices Generator API v0.0.1", 
+          "Our Generator creates invoices out of customers and items.", 
+          "Our terms of service are pretty loose", 
+          "Terms of service", 
+         
+          new Contact("Deimis Čekanauskas", "www.google.com", "tikras@email.com"), 
+          "License of API", "API license URL", Collections.emptyList());
     }
 }
  
