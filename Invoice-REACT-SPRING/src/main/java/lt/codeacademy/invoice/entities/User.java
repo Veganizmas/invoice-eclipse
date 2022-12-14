721ab0c93df7834368c2dc43180c1b16cc4b73e2
Krismarcus <markristupas@gmail.com>
2022-12-14 16:03:41 +0200

    Veikiantis Security, pridetos teises Swageriui.

--- Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/entities/User.java
+++ Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/entities/User.java
@@ -21,15 +21,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
+
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table( name = "users",
 uniqueConstraints = {
 		@UniqueConstraint(columnNames = "username"),
 		@UniqueConstraint(columnNames = "email")
 })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,6 +56,16 @@ public class User {
 			joinColumns = @JoinColumn(name = "user_id"), 
 			inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
+	public User(@NotBlank @Size(max = 20) String username, @NotBlank @Size(max = 50) @Email String email,
+			@NotBlank @Size(max = 120) String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;	
+	}
+	
+	
 	
 	
 
