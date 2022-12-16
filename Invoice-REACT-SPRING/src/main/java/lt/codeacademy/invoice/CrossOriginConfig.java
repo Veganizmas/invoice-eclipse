9a55594b7b871ca7f87b48e5043c758637cdf6c0
SpikisM <spukasmindaugas1@gmail.com>
2022-12-16 14:35:05 +0200

    komentaras

--- Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/CrossOriginConfig.java
+++ Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/CrossOriginConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//minaugo komentaras
 @Configuration
 public class CrossOriginConfig {
     @Bean
