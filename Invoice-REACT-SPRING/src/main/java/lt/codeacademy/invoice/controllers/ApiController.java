5a6429e6e7ab80d1d50240604defe31fc4a494b2
Laurynas.Bukys <bukys.laurynas@gmail.com>
2022-12-16 13:20:09 +0200

    preauthorized anottations for apicontroller endpoints

--- Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/controllers/ApiController.java
+++ Invoice-REACT-SPRING/src/main/java/lt/codeacademy/invoice/controllers/ApiController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,53 +53,63 @@ public class ApiController {
 
 	@ApiOperation(value = "Get all Customers list")
 	@GetMapping("/customers")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<Customer> getAllCustomers() {
 		return customerService.getCustomerList();
 	}
 
 	@PostMapping("/customers")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Customer saveCustomerDetails(@RequestBody Customer customer) {
 		System.out.println( customer );
 		return customerService.addCustomer( customer );
 	}
 
 	@PostMapping("/customers/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Customer updateCustomer(@RequestBody Customer customer, @PathVariable Long id) {
 		return customerService.updateCustomerById( id, customer );
 	}
 
 	@GetMapping("/customers/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Customer getCustomerById(@PathVariable Long id) {
 		return customerService.getCustomerById( id );
 	}
 
 	@DeleteMapping("/customers/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public ResponseEntity<HttpStatus> deleteCustomerById(@PathVariable Long id) {
 		customerService.deleteCustomerById( id );
 		return new ResponseEntity<>( HttpStatus.NO_CONTENT );
 	}
 
 	@GetMapping("/items")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<Item> getAllItems() {
 		return itemService.getItemList();
 	}
 
 	@PostMapping("/items")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Item saveItemDetails(@RequestBody Item item) {
 		return itemService.addItem( item );
 	}
 
 	@PostMapping("/items/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Item updateItem(@RequestBody Item item, @PathVariable Long id) {
 		return itemService.updateItemById( id, item );
 	}
 
 	@GetMapping("/items/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Item getItemById(@PathVariable Long id) {
 		return itemService.getItemById( id );
 	}
 
 	@DeleteMapping("/items/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public ResponseEntity<HttpStatus> deleteItemById(@PathVariable Long id) {
 		itemService.deleteItemById( id );
 		return new ResponseEntity<>( HttpStatus.NO_CONTENT );
@@ -107,26 +118,31 @@ public class ApiController {
 	//users endpoints
 	
 	@GetMapping("/users")
+	@PreAuthorize("hasRole('ADMIN')")
 	public List<User> getAllUsers() {
 		return userService.getUserList();
 	}
 
 	@PostMapping("/users")
+	@PreAuthorize("hasRole('ADMIN')")
 	public User saveUserDetails(@RequestBody User user) {
 		return userService.addUser( user );
 	}
 
 	@PostMapping("/users/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public User updateUser(@RequestBody User user, @PathVariable Long id) {
 		return userService.updateUserById( id, user );
 	}
 
 	@GetMapping("/users/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public User getUserById(@PathVariable Long id) {
 		return userService.getUserById( id );
 	}
 
 	@DeleteMapping("/users/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<HttpStatus> deleteUserById(@PathVariable Long id) {
 		userService.deleteUserById( id );
 		return new ResponseEntity<>( HttpStatus.NO_CONTENT );
@@ -135,12 +151,14 @@ public class ApiController {
 	//*****************
 
 	@GetMapping("/invoices")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<Invoice> getAllInvoices() {
 
 		return invoiceService.getInvoiceList();
 	}
 
 	@PostMapping("/invoices")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Invoice saveInvoiceDetails(@RequestBody Invoice invoice) {
 		return invoiceService.addInvoice( invoice );
 		// return invoice;
@@ -152,16 +170,19 @@ public class ApiController {
 	}
 
 	@PostMapping("/invoices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Invoice updateInvoice(@RequestBody Invoice invoice, @PathVariable Long id) {
 		return invoiceService.updateInvoiceById( id, invoice );
 	}
 
 	@GetMapping("/invoices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public Invoice getInvoiceById(@PathVariable Long id) {
 		return invoiceService.getInvoiceById( id );
 	}
 
 	@DeleteMapping("/invoices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public ResponseEntity<HttpStatus> deleteInvoiceById(@PathVariable Long id) {
 		invoiceService.deleteInvoiceById( id );
 		return new ResponseEntity<>( HttpStatus.NO_CONTENT );
